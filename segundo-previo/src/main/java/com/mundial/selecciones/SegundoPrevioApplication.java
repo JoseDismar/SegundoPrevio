d3a09dd1e5c73c06de007ebb202e4e32582bb5a1
Jose Dismar <josedismar@gmail.com>
2023-05-29 10:43:44 -0500

    primer commmit

---
+++ segundo-previo/src/main/java/com/mundial/selecciones/SegundoPrevioApplication.java
@@ -0,0 +1,13 @@
+package com.mundial.selecciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoPrevioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoPrevioApplication.class, args);
+	}
+
+}
---
+++ segundo-previo/src/test/java/com/mundial/selecciones/SegundoPrevioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mundial.selecciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoPrevioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
